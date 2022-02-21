937df93df108da908c451cbd2f969b7bb6953a35
burakugras <99265170+burakugras@users.noreply.github.com>
2022-02-21 23:18:42 +0300

    Add files via upload
    c# temel try catch kullanımı

---
+++ trycatchPratik/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace trycatchPratik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Console.WriteLine("Bir sayı giriniz: ");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Girdiğini sayı: " + sayi);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Hata: " + ex.Message.ToString());
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("İşlem tamamlandı.");
+            //}
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-20000000000000000");
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok büyük yada çok küçük bir değer girdiniz");
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
